e99915705ec105f4a22ab08c11eb42c71834385d
Neahal Ahmad <nehalahmad276@gmail.com>
2026-02-23 17:14:15 +0530

    token

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -178,6 +178,8 @@ router.post('/login', async (req, res) => {
       req.body
     );
 
+    const token = response.data.token;
+
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
