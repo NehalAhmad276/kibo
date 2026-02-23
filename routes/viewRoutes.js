d8d5624183d4c1c140a2dcb88243833f7b8f6e36
Neahal Ahmad <nehalahmad276@gmail.com>
2026-02-23 16:56:41 +0530

    path

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -182,7 +182,8 @@ router.post('/login', async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: 'none',
-      domain: 'xn--kib-sxa.com',
+      // domain: 'xn--kib-sxa.com',
+      path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
